2d33cd3770de91053e407eaac95cb76a8a05767f
YashavikaSingh <65505787+YashavikaSingh@users.noreply.github.com>
2025-04-28 22:33:20 -0400

    made changes

--- lambdas/index-photos.py
+++ lambdas/index-photos.py
@@ -8,7 +8,7 @@ import urllib.parse
 s3 = boto3.client('s3')
 rekognition = boto3.client('rekognition')
 
-print("DEPLOYING LAMBDA VIA CODEBUILD-2")
+print("DEPLOYING LAMBDA VIA CODEBUILD-3")
 # OpenSearch Configuration
 OPENSEARCH_ENDPOINT = "https://search-photos-2zcv2zir5roxmzmvvt7ezajhyi.us-east-1.es.amazonaws.com/photos/_doc"
 OPENSEARCH_USERNAME = "yashavika"
